c85061d75715d879e58063d3598e04b6b0ffa428
nahir1009 <nahir1009@gmail.com>
2024-11-03 16:51:18 -0300

    agrego grafico circular encendidos

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const BASE_URL = 'https://serverwokwi.onrender.com';
 const GET_DATA_PATH = '/get_data';
 const tableBody = document.getElementById('ledData');
-const refreshButton = document.getElementById('refreshButton'); 
+const refreshButton = document.getElementById('refreshButton');
+const ledStatusChartCanvas = document.getElementById('ledStatusChart').getContext('2d');
+
+let ledStatusChart;
+
+// Configuración inicial del gráfico de estado de LEDs
+const setupChart = () => {
+    const data = {
+        labels: ['Encendido', 'Apagado'],
+        datasets: [{
+            data: [0, 0],
+            backgroundColor: ['rgb(0, 255, 0)', 'rgb(255, 0, 0)'], 
+            hoverOffset: 4
+        }]
+    };
+
+    ledStatusChart = new Chart(ledStatusChartCanvas, {
+        type: 'doughnut',
+        data: data
+    });
+};
+
+
+const updateChart = (data) => {
+    const onCount = data.filter(row => row.led_status).length;
+    const offCount = data.length - onCount;
+
+    ledStatusChart.data.datasets[0].data = [onCount, offCount];
+    ledStatusChart.update();
+};
+
+
 const fetchLedData = async () => {
     const API_URL = `${BASE_URL}${GET_DATA_PATH}`;
-    const FETCH_OPTIONS = { 
-        method: 'GET'
-    };
+    const FETCH_OPTIONS = { method: 'GET' };
 
     console.log("El fetch se inicia");
     try {
@@ -20,11 +49,13 @@ const fetchLedData = async () => {
         console.log("Datos obtenidos:", data);
 
         renderTable(data);
+        updateChart(data); 
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 };
 
+// Función para renderizar la tabla de datos de LEDs
 const renderTable = (data) => {
     tableBody.innerHTML = '';
 
@@ -51,7 +82,9 @@ const renderTable = (data) => {
     });
 };
 
+
 const main = () => {
+    setupChart();
     window.onload = fetchLedData;
     refreshButton.addEventListener('click', (event) => {
         event.preventDefault();
